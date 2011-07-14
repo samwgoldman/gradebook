1351790c47a34d7086fec80486b39be8fc9de229
Sam Goldman <samwgoldman@gmail.com>
2011-07-14 01:30:44 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gradebook::Application.config.secret_token = '5b4964b421a81800380be24a4a8f5585f4a1062ed5d93943193c1820e93dd7224c74d9806148d5dc47a66d913f3da0328127615a111520b24ad2115dfee9774b'

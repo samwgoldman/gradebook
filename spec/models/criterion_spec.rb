2b2719f7eb7bae5d15f987f5ca85acb86414a53f
Sam Goldman <samwgoldman@gmail.com>
2011-07-25 00:22:40 -0400

    Fully test criterion.

--- spec/models/criterion_spec.rb
+++ spec/models/criterion_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Criterion do
   it { should belong_to(:evaluation) }
+  it { should have_many(:alternatives) }
+  it { should accept_nested_attributes_for(:alternatives).allow_destroy(true) }
   it { should validate_presence_of(:evaluation) }
   it { should validate_presence_of(:prompt) }
 end

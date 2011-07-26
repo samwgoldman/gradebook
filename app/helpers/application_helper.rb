3b143f8a07bd232ca93e7aa33d3cc627be6045fb
Sam Goldman <samwgoldman@gmail.com>
2011-07-26 00:35:52 -0400

    Refactor views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_wrapper(&block)
+    content_tag :section, capture(&block)
+  end
+
+  def flashes
+    flash.keys.map { |key| flash_tag key }.join.html_safe
+  end
+
+  def flash_tag(key)
+    content_tag :p, flash[key], :class => key
+  end
 end

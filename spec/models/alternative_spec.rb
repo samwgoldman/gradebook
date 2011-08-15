ff520339c5e11c5a44e7e0ecaca0c8263327d47d
Sam Goldman <samwgoldman@gmail.com>
2011-08-15 15:35:46 -0400

    Refactor unit tests.

--- spec/models/alternative_spec.rb
+++ spec/models/alternative_spec.rb
@@ -2,6 +2,5 @@ require 'spec_helper'
 
 describe Alternative do
   it { should belong_to(:criterion) }
-  it { should validate_presence_of(:criterion) }
-  it { should validate_presence_of(:label) }
+  it { should validate_presence_of(:criterion, :label) }
 end
--- spec/models/criterion_spec.rb
+++ spec/models/criterion_spec.rb
@@ -4,6 +4,5 @@ describe Criterion do
   it { should belong_to(:evaluation) }
   it { should have_many(:alternatives) }
   it { should accept_nested_attributes_for(:alternatives).allow_destroy(true) }
-  it { should validate_presence_of(:evaluation) }
-  it { should validate_presence_of(:prompt) }
+  it { should validate_presence_of(:evaluation, :prompt) }
 end
